f313a2cd73f6755fde2efe908679d7cdf350cf59
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2020-11-25 16:24:09 +0900

    サービスプロバイダ練習

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        # 商用環境以外だった場合にSQLログを出力させます
+        if (config('app.env') !== 'production' ) {
+            DB::listen(function ($query) {
+                Log::info("Query time:{$query->time}s] $query->sql");
+            });
+        }
     }
 }
